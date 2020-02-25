b6703f0509a739f0f748487f8cc79b6faadbb8e3
Michael Gielda <mgielda@antmicro.com>
2020-02-25 09:37:28 +0100

    Implement sane defaults for dataclass

--- tuttest/__init__.py
+++ tuttest/__init__.py
@@ -23,7 +23,7 @@ def parse_rst(text: str, names: Optional[List[str]]) -> OrderedDict:
     literal_blocks = doctree.traverse(condition=is_literal_block)
 
     for idx, block in enumerate(literal_blocks):
-        snippet = Snippet(block.astext(), '', {})
+        snippet = Snippet(block.astext())
 
         name = ''
         name = ' '.join(block['names'])
@@ -46,7 +46,7 @@ def parse_markdown(text: str, names: Optional[List[str]]) -> OrderedDict:
     lines = text.split('\n')
 
     inside = False
-    snippet = Snippet('', '', {})
+    snippet = Snippet()
 
     # TODO: fix for indented code blocks e.g. inside lists
 
@@ -66,7 +66,7 @@ def parse_markdown(text: str, names: Optional[List[str]]) -> OrderedDict:
             else:
                 # we're starting to parse a new snippet
                 inside = True
-                snippet = Snippet('', '', {})
+                snippet = Snippet()
                 snippet_lines = []
                 lang = l[3:].strip()
                 if lang != "":
@@ -88,7 +88,7 @@ def parse_markdown(text: str, names: Optional[List[str]]) -> OrderedDict:
         prevl = l
     return snippets
 
-def get_snippets(filename: str, names: Optional[List[str]] = None) -> OrderedDict:
+def get_snippets(filename: str, names: Optional[List[str]] = None, parse: bool = False) -> OrderedDict:
 
     text = open(filename).read()
     snippets = None
@@ -96,21 +96,25 @@ def get_snippets(filename: str, names: Optional[List[str]] = None) -> OrderedDic
         snippets = parse_rst(text, names)
     else: # the default is markdown
         snippets = parse_markdown(text, names)
+    if parse:
+        for s in snippets.values():
+            s.commands = parse_snippet(s.text)
     return snippets
 
-from dataclasses import dataclass
-
-@dataclass
-class Snippet:
-    text: List[str]
-    lang: str
-    meta: Dict
+from dataclasses import dataclass, field
 
 @dataclass
 class Command:
     prompt: str
     command: str
     result: str = ''
+
+@dataclass
+class Snippet:
+    text: List[str] = field(default_factory=list)
+    lang: str = ''
+    meta: Dict = field(default_factory=dict)
+    commands: List[Command] = field(default_factory=list)
 
 # currently only parse Renode snippets
 def parse_snippet(snippet: str) -> List[Command]:
